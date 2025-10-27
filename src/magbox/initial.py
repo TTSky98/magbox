b0a5e0e3c46fdcd9312934f979dac4c70025f360
wyt <20110190052@fudan.edu.cn>
2025-10-28 03:02:39 +0800

    bug fix: 默认值

--- src/magbox/initial.py
+++ src/magbox/initial.py
@@ -1,20 +1,18 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Union
 
 @dataclass
 class Lattice:
-    size: list
-    type: str
-    periodic: Union[bool , list[bool]] = True
-    J_direction: Union[int, None] = None
+    size: list = field(default_factory=list)
+    type: str = field(default_factory=str)
+    periodic: Union[bool, list[bool]] = field(default_factory=lambda: True)
+    J_direction: Union[int, None] = field(default_factory=lambda: None)
 
 @dataclass
 class Vars:
-    B :float=0.0
-    B_dir :list[float]= [0.0,0.0,1.0]
-
-    J :Union[float,list[float]] =1.0
-    K1_dir :list[float] = [0.0,0.0,1.0]
-    K1 :float=1.0
-
-    D :float=0.0
+    B: float = field(default_factory=lambda: 0.0)
+    B_dir: list[float] = field(default_factory=lambda: [0.0, 0.0, 1.0])
+    J: Union[float, list[float]] = field(default_factory=lambda: 1.0)
+    K1_dir: list[float] = field(default_factory=lambda: [0.0, 0.0, 1.0])
+    K1: float = field(default_factory=lambda: 1.0)
+    D: float = field(default_factory=lambda: 0.0)
